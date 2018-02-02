ec950e774cf63a2931a36a925be152f7288c95b0
Desprez Thibault <thibault.desprez1@gmail.com>
2018-02-02 18:50:58 +0100

    update pince work exemple for the open doors
    and some photos...

--- pince/pince_ino/pince_ino.ino
+++ pince/pince_ino/pince_ino.ino
@@ -2,11 +2,12 @@
 
 #define debugPin 22
 #define bouton 30
-#define potar A2
+#define rotorButton 36
 
 
 int val=10;    // variable to read the value from the analog pin 
 int valRotor = 90;
+int rotorAngle = 0;
 bool boutonTrigger = false;
 CDF_pince pince(9,11,22);   
 
@@ -15,22 +16,31 @@ void setup()
 {   
 	pinMode(debugPin, INPUT);
 	pinMode(bouton, INPUT);
-
+  pinMode(rotorButton, INPUT);
+	
 	Serial.begin(9600);
 	Serial.print("Début");
 } 
 
 void loop() 
 {  
-	valRotor = analogRead(potar);
-	valRotor = map(valRotor, 0,1024, 1,179);
-	pince.rot(valRotor);
-
+	if(digitalRead(rotorButton)){
+    while(digitalRead(rotorButton));
+    if(rotorAngle == 0){
+      rotorAngle = 1;
+      pince.rot(1);
+    }else if(rotorAngle==1){
+      rotorAngle = 2;
+      pince.rot(90);
+    }else{
+      rotorAngle = 0;
+      pince.rot(179);
+    }
+	}
+  
 	if(digitalRead(bouton)){
 		boutonTrigger = !boutonTrigger;
-		do{
-			;
-		}while(digitalRead(bouton));
+		while(digitalRead(bouton));
 
 		if(boutonTrigger){
 			pince.autoGrab();
